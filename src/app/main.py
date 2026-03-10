6e29eaf3f44389ee0df041e4c58b41984e4ae8ca
khzmalika <maikhaziakhmetova@kpfu.ru>
2026-03-10 18:26:55 +0300

    feat(db): db sqlite models (#4)
    Co-authored-by: khazmalika <khazmalika@gmail.com>

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from .models import create_db_and_tables
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get('/')
 def read_root():
