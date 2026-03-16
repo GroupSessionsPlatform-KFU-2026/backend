880fdbf05ff7a333a97f3e0dc90a0f57a574c8d1
xludw1ng <109303407+xludw1ng@users.noreply.github.com>
2026-03-16 11:23:37 +0300

    feat: add initial openapi specification (#9)
    Co-authored-by: khzmalika <maikhaziakhmetova@kpfu.ru>
    Co-authored-by: khazmalika <khazmalika@gmail.com>

--- src/app/models.py
+++ src/app/models.py
@@ -1,9 +1,10 @@
-from typing import Annotated
 from datetime import datetime
+from typing import Annotated
 from uuid import UUID, uuid4
 
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends
+from sqlmodel import Field, Session, SQLModel, create_engine
+
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -15,14 +16,16 @@ class User(SQLModel, table=True):
     last_login_at: datetime | None = None
     is_active: bool
 
+
 class Project(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    owner_id: int = Field(foreign_key="user.id")
+    owner_id: int = Field(foreign_key='user.id')
     title: str
-    description: str | None=None
+    description: str | None = None
     created_at: datetime
     updated_at: datetime | None = None
     is_archived: bool
+
 
 class Tag(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -34,26 +37,27 @@ class Tag(SQLModel, table=True):
 
 class ProjectTag(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    project_id: int = Field(foreign_key="project.id")
-    tag_id: int = Field(foreign_key="tag.id")
+    project_id: int = Field(foreign_key='project.id')
+    tag_id: int = Field(foreign_key='tag.id')
     created_at: datetime
     is_active: bool
 
 
 class Room(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
-    project_id: int = Field(foreign_key="project.id")
-    creator_id: int = Field(foreign_key="user.id")
+    project_id: int = Field(foreign_key='project.id')
+    creator_id: int = Field(foreign_key='user.id')
     title: str
     room_code: str = Field(index=True)
     created_at: datetime
     ended_at: datetime | None = None
     status: str
 
+
 class RoomParticipant(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    room_id: UUID = Field(foreign_key="room.id")
-    user_id: int = Field(foreign_key="user.id")
+    room_id: UUID = Field(foreign_key='room.id')
+    user_id: int = Field(foreign_key='user.id')
     role: str
     joined_at: datetime
     left_at: datetime | None = None
@@ -62,8 +66,8 @@ class RoomParticipant(SQLModel, table=True):
 
 class ChatMessage(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    room_id: UUID = Field(foreign_key="room.id")
-    sender_id: int = Field(foreign_key="user.id")
+    room_id: UUID = Field(foreign_key='room.id')
+    sender_id: int = Field(foreign_key='user.id')
     content: str
     sent_at: datetime
     is_edited: bool
@@ -71,10 +75,10 @@ class ChatMessage(SQLModel, table=True):
 
 class BoardElement(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    room_id: UUID = Field(foreign_key="room.id")
-    author_id: int = Field(foreign_key="user.id")
+    room_id: UUID = Field(foreign_key='room.id')
+    author_id: int = Field(foreign_key='user.id')
     element_type: str
-    data: str               
+    data: str
     created_at: datetime
     updated_at: datetime | None = None
     is_deleted: bool
@@ -82,15 +86,16 @@ class BoardElement(SQLModel, table=True):
 
 class BoardElementComment(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    board_element_id: int = Field(foreign_key="boardelement.id")
-    author_id: int = Field(foreign_key="user.id")
+    board_element_id: int = Field(foreign_key='boardelement.id')
+    author_id: int = Field(foreign_key='user.id')
     content: str
     is_deleted: bool
     created_at: datetime
 
+
 class PomodoroSession(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    room_id: UUID = Field(foreign_key="room.id")
+    room_id: UUID = Field(foreign_key='room.id')
     work_duration: int
     short_break_duration: int
     long_break_duration: int
@@ -102,15 +107,17 @@ class PomodoroSession(SQLModel, table=True):
     is_running: bool
     last_updated_at: datetime
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False}
+sqlite_file_name = 'db.sqlite3'
+sqlite_url = f'sqlite:///{sqlite_file_name}'
+
+connect_args = {'check_same_thread': False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
