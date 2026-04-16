3d353b538ffbe873460778d2b802f3283a9bec44
xludw1ng <xludwingcerda@gmail.com>
2026-04-16 19:04:59 +0300

    feature: added socket's event -> board_commnets

--- src/app/sockets/events/__init__.py
+++ src/app/sockets/events/__init__.py
@@ -1,4 +1,5 @@
 from src.app.sockets.events.board import register_board_events
+from src.app.sockets.events.board_comments import register_board_comment_events
 from src.app.sockets.events.chat import register_chat_events
 from src.app.sockets.events.presence import (
     emit_participant_joined,
@@ -8,6 +9,7 @@ from src.app.sockets.events.presence import (
 
 __all__ = [
     'register_board_events',
+    'register_board_comment_events',
     'register_chat_events',
     'emit_participant_joined',
     'emit_participant_left',
