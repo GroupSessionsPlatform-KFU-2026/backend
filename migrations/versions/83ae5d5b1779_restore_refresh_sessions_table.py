134e4135cee94f994a60807123d36afabfd4d87c
xludw1ng <xludwingcerda@gmail.com>
2026-04-27 23:53:58 +0300

    fix: linter + pomodoro settings

--- migrations/versions/83ae5d5b1779_restore_refresh_sessions_table.py
+++ migrations/versions/83ae5d5b1779_restore_refresh_sessions_table.py
@@ -6,11 +6,6 @@ Create Date: 2026-04-09 21:31:15.097741
 
 """
 
-import sqlalchemy as sa
-from alembic import op
-from sqlalchemy.dialects import postgresql
-
-# revision identifiers, used by Alembic.
 revision = '83ae5d5b1779'
 down_revision = 'b95b11e272cf'
 branch_labels = None
@@ -18,43 +13,8 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
-        'refresh_sessions',
-        sa.Column('id', sa.Uuid(), nullable=False),
-        sa.Column('created_at', postgresql.TIMESTAMP(timezone=True), nullable=False),
-        sa.Column('updated_at', postgresql.TIMESTAMP(timezone=True), nullable=False),
-        sa.Column('user_id', sa.Uuid(), nullable=False),
-        sa.Column('jti', sa.String(length=255), nullable=False),
-        sa.Column('expires_at', postgresql.TIMESTAMP(timezone=True), nullable=False),
-        sa.Column(
-            'is_revoked', sa.Boolean(), nullable=False, server_default=sa.text('false')
-        ),
-        sa.ForeignKeyConstraint(
-            ['user_id'], ['user.id'], name=op.f('refresh_sessions_user_id_fkey')
-        ),
-        sa.PrimaryKeyConstraint('id', name=op.f('refresh_sessions_pkey')),
-        sa.UniqueConstraint(
-            'jti',
-            name=op.f('refresh_sessions_jti_key'),
-            postgresql_include=[],
-            postgresql_nulls_not_distinct=False,
-        ),
-    )
-    op.create_index(
-        op.f('ix_refresh_sessions_user_id'),
-        'refresh_sessions',
-        ['user_id'],
-        unique=False,
-    )
-    op.create_index(
-        op.f('ix_refresh_sessions_jti'),
-        'refresh_sessions',
-        ['jti'],
-        unique=True,
-    )
+    pass
 
 
 def downgrade() -> None:
-    op.drop_index(op.f('ix_refresh_sessions_jti'), table_name='refresh_sessions')
-    op.drop_index(op.f('ix_refresh_sessions_user_id'), table_name='refresh_sessions')
-    op.drop_table('refresh_sessions')
+    pass
